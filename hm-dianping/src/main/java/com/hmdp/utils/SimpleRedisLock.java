6c9efc6f7beb662b022709325ada42e26c15ed15
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2026-02-27 17:14:50 +1100

    通过添加和判断线程标识解决redis分布式锁误删的问题

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,13 +1,14 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
 
-    private StringRedisTemplate stringRedisTemplate;
-    private String name;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final String name;
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -15,11 +16,12 @@ public class SimpleRedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取线程标识
-        String threadId = Thread.currentThread().getId() + "";
+        // 获取线程标识(UUID拼接线程id)
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success); // 避免自动拆箱null导致空指针
@@ -27,7 +29,13 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
+        // 获取线程表示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 判断标识是否一致
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
         // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        if(threadId.equals(id)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
